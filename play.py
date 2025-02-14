bd5497783c47e9805935ee76c1c26e623468317e
K00lman <meepsterIQ@yahoo.com>
2025-02-14 12:08:04 -0800

    Added basic file structure

---
+++ play.py
@@ -0,0 +1,2 @@
+"""This file contains the code for the play state of a character. The play state is a interactive character sheet
+designed to used to keep track of character abilities during a game session"""
---
+++ ui.py
@@ -0,0 +1,9 @@
+"""This file contains all the required code to run and manage the UI across the whole codebase"""
+
+import tkinter as tk
+
+# Startup UI
+
+# Creator UI
+
+# Play UI
